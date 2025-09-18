8fc4e3d71d5c52c2b705128416ea9216865ffdef
ahhmedsamii77 <ahhmedsamii77@gmail.com>
2025-09-18 15:14:45 +0300

    multer

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,10 +1,17 @@
 import multer from "multer";
-
+import os from "node:os";
 export const allowedExtension = {
   image: ["image/png", "image/webp", "image/jpeg", "image/gif", "image/png"]
 }
 export function Multer(cusomExtension = []) {
-  const storage = multer.diskStorage({});
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, os.tmpdir());
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${nanoid()}-${file.originalname}`)
+    }
+  });
   function fileFilter(req, file, cb) {
     if (!cusomExtension.includes(file.mimetype)) {
       cb(new Error("Invalid file type"));
