11ca40ec5cfb91cc6ca899ad146de54f1c910d74
ahhmedsamii77 <ahhmedsamii77@gmail.com>
2025-09-20 21:07:35 +0300

    image

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 import { nanoid } from "nanoid";
 import os from "node:os";
 export const allowedExtension = {
-  image: ["image/png", "image/webp", "image/jpeg", "image/gif", "image/png"]
+  image: ["image/png", "image/webp", "image/jpeg", "image/gif", "image/png" , "image/jpg"],
 }
 export function Multer(cusomExtension = []) {
   const storage = multer.diskStorage({
